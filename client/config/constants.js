b41b679bb0dc8d3f8955b5062e0ec4531a67483e
Andrew Dinh <andrewdinh74@yahoo.com>
2015-05-13 23:01:18 -0700

    Remove unnecessary files

--- client/config/constants.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('poseidon')
-.constant('firebaseUrl', 'https://__________.firebaseio.com/')
-.constant('nodeUrl', 'http://localhost:8000');
--- client/config/run.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('poseidon')
-.run(function($rootScope, $window, $firebaseAuth, firebaseUrl){
-  $rootScope.fbRoot = new $window.Firebase(firebaseUrl);
-  $rootScope.afAuth = $firebaseAuth($rootScope.fbRoot);
-});
