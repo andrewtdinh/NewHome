1d417bd1bc4bfafac9164f6d3082bd8d1b1b7738
Andrew Dinh <andrewdinh74@yahoo.com>
2015-06-15 17:32:11 -0700

    able to get card to flip

--- client/index.js
+++ client/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+$(document).ready(init);
+
+function init(){
+  $('.card').hover(flipForth, flipBack);
+}
+
+function flipForth(){
+  $(this).addClass('flipped');
+}
+
+function flipBack(){
+  $(this).removeClass('flipped');
+}
